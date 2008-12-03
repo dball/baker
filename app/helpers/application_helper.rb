8d9b518161b8fc5d0f33977fb135abdec6066012
Donald Ball <donald.ball@gmail.com>
2008-12-02 23:33:56 -0600

    adding layout and helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title(title)
+    content_for(:title) { title }
+  end
+
+  def javascript(*files)
+    content_for(:head) { javascript_include_tag(*files) }
+  end
+
+  def stylesheet(*files)
+    content_for(:head) { stylesheet_link_tag(*files) }
+  end
 end
