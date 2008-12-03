cac327f43d67ba43c536185f2f9206ca309cc58e
Donald Ball <donald.ball@gmail.com>
2008-12-03 17:26:55 -0600

    adding pseudo attributes for bulk assignment of ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,26 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
+
+  def new_ingredient_attributes=(ingredient_attributes)
+    ingredient_attributes.each do |attributes|
+      ingredients.build(attributes)
+    end
+  end
+
+  def existing_ingredient_attributes=(ingredient_attributes)
+    ingredients.reject(&:new_record?).each do |ingredient|
+      if attributes = ingredient_attributes[ingredient.id.to_s]
+        ingredient.attributes = attributes
+      else
+        basket.ingredients.delete(ingredient)
+      end
+    end
+  end
+
+  after_update do
+    ingredients.each { |ingredient| ingredient.save(false) }
+  end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -9,6 +9,36 @@ describe Recipe do
     it "must have ingredients" do
       @recipe.ingredients.map {|i| i.class }.all? {|c| c == Ingredient }.should be_true
     end
+
+    it "should build new ingredients via new_ingredient_attributes" do
+      attributes = (1..3).to_a.map do
+        Ingredient.spawn.attributes.reject {|k, v| k == 'recipe_id' }
+      end
+      old_ingredient_attributes = @recipe.ingredients.map {|i| i.attributes }
+      @recipe.new_ingredient_attributes = attributes
+      @recipe.ingredients.map {|i| i.attributes }.should ==
+        old_ingredient_attributes + 
+        attributes.map {|attr| attr.merge({ 'recipe_id' => @recipe.id }) }
+    end
+  end
+
+  describe "a valid saved recipe", :shared => true do
+    it "must have no errors" do
+      @recipe.errors.should be_empty
+    end
+
+    it "must have a unique name" do
+      Recipe.generate(:name => @recipe.name).errors.on(:name).should == 
+        'has already been taken'
+    end
+  end
+
+  describe "an empty unsaved recipe" do
+    before(:each) do
+      @recipe = Recipe.spawn
+    end
+
+    it_should_behave_like "all recipes"
   end
 
   describe "an empty recipe" do
@@ -17,14 +47,25 @@ describe Recipe do
     end
     
     it_should_behave_like "all recipes"
+    it_should_behave_like "a valid saved recipe"
   end
 
-  describe "with two ingredients" do
+  describe "an unsaved recipe with ingredients" do
     before(:each) do
-      @recipe = Recipe.generate
-      2.times { @recipe.ingredients << Ingredient.generate }
+      @recipe = Recipe.spawn
+      3.times { @recipe.ingredients << Ingredient.spawn }
     end
 
     it_should_behave_like "all recipes"
+  end
+
+  describe "a recipe with ingredients" do
+    before(:each) do
+      @recipe = Recipe.generate
+      3.times { @recipe.ingredients << Ingredient.generate }
+    end
+
+    it_should_behave_like "all recipes"
+    it_should_behave_like "a valid saved recipe"
   end
 end
