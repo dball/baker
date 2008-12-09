6b01a89d6ecab0809ace6e6d43136e57ddae283b
Donald Ball <donald.ball@gmail.com>
2008-12-09 01:23:00 -0600

    dynamic adding of ingredients

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function addIngredient(input) {
+  var ingredient = $(input).parents('.ingredient');
+  var copy = ingredient.clone();
+  copy.find('.deleter').show();
+  copy.find('.adder').remove();
+  ingredient.find(':text').val('');
+  ingredient.before(copy);
+}
+
+function deleteIngredient(input) {
+  $(input).parents('.ingredient').remove();
+}
