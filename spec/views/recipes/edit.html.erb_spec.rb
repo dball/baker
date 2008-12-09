026964cdb185e37129df9048f3da2eb23f85d330
Donald Ball <donald.ball@gmail.com>
2008-12-08 23:04:33 -0600

    testing for name input and label underneath a posting form

--- spec/views/recipes/edit.html.erb_spec.rb
+++ spec/views/recipes/edit.html.erb_spec.rb
@@ -6,7 +6,10 @@ describe 'recipes/edit.html.erb' do
     render "/recipes/edit"
   end
 
-  it "should have an edit name field" do
-    response.should have_tag('input#recipe_name')
+  it "should have an edit form" do
+    response.should have_tag("form[method='post']") do
+      response.should have_tag("label[for='recipe_name']")
+      response.should have_tag("input#recipe_name[name='recipe[name]']")
+    end
   end
 end
