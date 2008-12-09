b1278388e821015b5a6972c7cc058a021adecc9e
Donald Ball <donald.ball@gmail.com>
2008-12-09 14:48:10 -0600

    overriding enter presses to act like tabs or add ingredient events

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,8 +7,31 @@ function addIngredient(input) {
   copy.find('.adder').remove();
   ingredient.find(':text').val('');
   ingredient.before(copy);
+  $(input).parents('tr').find(':text')[0].focus();
 }
 
 function deleteIngredient(input) {
   $(input).parents('.ingredient').remove();
 }
+
+function keyDownIngredient(event) {
+  if (event.keyCode == 13) {
+    var inputs = $('#ingredients').find(':text');
+    for (var i=0; i<inputs.length; i++) {
+      if (inputs[i] == this) {
+        if (i + 1 == inputs.length) {
+          addIngredient(this);
+        } else {
+          nextInput = inputs[i + 1];
+          nextInput.focus();
+          nextInput.select();
+        }
+      }
+    }
+    return false;
+  }
+}
+
+$(document).ready(function() {
+  $('#ingredients').find(':text').keydown(keyDownIngredient);
+});
