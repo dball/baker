1878cbb9f24efd54822ce4fdcf2052782730b377
Donald Ball <donald.ball@gmail.com>
2008-12-09 23:41:17 -0600

    adding quantity converter

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,3 +1,8 @@
 class Unit < ActiveRecord::Base
   validates_numericality_of :scale, :greater_than => 0
+
+  def convert_quantity_to(quantity, unit)
+    raise ArgumentError unless kind == unit.kind
+    quantity * unit.scale / scale
+  end
 end
--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -30,4 +30,20 @@ describe Unit do
     
     it_should_behave_like "all units"
   end
+
+  describe "converting between units" do
+    before(:each) do
+      @unit = Unit.generate
+    end
+
+    it "should convert to a unit of the same kind" do
+      unit = Unit.generate({ :kind => @unit.kind, :scale => 2 })
+      unit.convert_quantity_to(10, @unit).should == 5
+    end
+
+    it "should not convert to a unit of another kind" do
+      unit = Unit.generate({ :kind => 'foo', :scale => 2 })
+      lambda { unit.convert_quantity_to(10, @unit) }.should raise_error(ArgumentError)
+    end
+  end
 end
