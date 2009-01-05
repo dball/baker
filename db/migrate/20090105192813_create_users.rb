dce4d3b992c0669b57c8fb10529bc20995db60e9
Donald Ball <donald.ball@gmail.com>
2009-01-05 15:22:42 -0600

    adding null constraints, unique index to users

--- db/migrate/20090105192813_create_users.rb
+++ db/migrate/20090105192813_create_users.rb
@@ -1,22 +1,23 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.string :login
-      t.string :crypted_password
-      t.string :password_salt
-      t.string :persistence_token
-      t.integer :login_count
+      t.string :login, :null => false
+      t.string :crypted_password, :null => false
+      t.string :password_salt, :null => false
+      t.string :persistence_token, :null => false
+      t.integer :login_count, :null => false, :default => 0
       t.datetime :last_request_at
       t.datetime :last_login_at
       t.datetime :current_login_at
       t.string :last_login_ip
       t.string :current_login_ip
-
       t.timestamps
     end
+    add_index :users, :login, :unique => true
   end
 
   def self.down
+    remove_index :users, :login
     drop_table :users
   end
 end
